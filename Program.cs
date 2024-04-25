a37801e09a3cea92a91fba123029ce7075c308c6
Sergei Belov <serbel95@mail.ru>
2024-04-25 20:45:50 +0300

    Создали функцию для создания нового массива состоящего из строк состоящих из трёх и менее элементов

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿string[] str = { "ewff", "dcwfe", "fs", "123", "saf" };
+﻿string NewArray(string[] arr)
+{
+    string result = "";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        // string result = "";
+        string found = arr[i];
+        int Length = found.Length;
+
+        if (Length <= a)
+        {
+
+            System.Console.Write($"{found}  ");
+        }
+
+
+    }
+    return result;
+}
+
+
+
+string[] str = { "ewff", "dcwfe", "fs", "123", "saf" };
